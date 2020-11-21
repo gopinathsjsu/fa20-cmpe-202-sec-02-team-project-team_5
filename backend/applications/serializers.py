b9b235dd91a992974dae345a023ad6a5d27fdb28
Deepthi-Techhub <jdeepthi1612@gmail.com>
2020-11-21 02:50:16 -0800

    Modified core and application models

--- backend/applications/serializers.py
+++ backend/applications/serializers.py
@@ -1,8 +1,19 @@
 from applications.models import *
 from rest_framework import serializers
+from core.models import User,UserAdditionalInfo
+from core.serializers import UserAdditionalInfoSerializer
 
+
+
+
+class SubmitApplicationSerializer(serializers.ModelSerializer):
+    user_info = UserAdditionalInfoSerializer()
+    class Meta:
+        model = Application
+        fields = ('home_listing')
 
 class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Application
         fields = '__all__'
+# class 
--- backend/applications/views.py
+++ backend/applications/views.py
@@ -2,6 +2,7 @@ import json
 import jwt
 from core.models import *
 from core.serializers import *
+from applications.serializers import *
 from django.conf import settings
 from rest_framework import status
 from django.core import serializers
@@ -11,3 +12,21 @@ from core.authentications import JWTAuthentication
 from django.http.response import JsonResponse
 from rest_framework.generics import GenericAPIView,CreateAPIView,ListAPIView
 from django.contrib.auth.hashers import make_password,check_password
+
+
+class SubmitApplicationView(GenericAPIView):
+    """
+    This view is created to allow user to submit a rental or buying application for specific home listing.
+    """
+    def post(self, request):
+        user = JWTAuthentication.validate_token(request)
+        serializer = SubmitApplicationSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            serializer.save()
+            user_application = Application.objects.get(user=user.id)
+            serializer = ApplicationSerializer(user_application)
+            response_data = {'message':'Application submission successful','application_details': serializer.data}
+            return JsonResponse(response_data, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
