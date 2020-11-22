f740e27b5ff56b621f79a61da64b3acb6a780f22
Deepthi-Techhub <jdeepthi1612@gmail.com>
2020-11-22 03:01:28 -0800

    Core Serializer changes name-id

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -5,6 +5,7 @@ It helps in data flow in Json formart to and fro communication (Json to Python a
 """
 from core.models import *
 from rest_framework import serializers
+from django.contrib.postgres.fields.citext import CIEmailField
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,22 +17,52 @@ class UserStatusSerializer(serializers.ModelSerializer):
         model = UserStatus
         fields = '__all__'
 
+class RoleFieldSerializer(serializers.Field):
+    def to_internal_value(self, value):
+        return Role.objects.get(name=value)
+
+class UserTypeFieldSerializer(serializers.Field):
+    def to_internal_value(self, value):
+        return UserType.objects.get(name=value)
+        
+
+class UserStatusFieldSerializer(serializers.Field):
+    def to_internal_value(self, value):
+        return UserStatus.objects.get(name=value)
+
+
 class UserSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    role = RoleFieldSerializer(required=False)
+    user_status = UserStatusFieldSerializer(required=False)
+    user_type = UserTypeFieldSerializer(required=False)
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email_id = CIEmailField()
+    password = serializers.CharField()
+
     class Meta:
         model = User
         fields = '__all__' 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    role = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name')
+    user_status = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name')
+    user_type = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name')
     class Meta:
         model = User
-        fields = ('first_name','last_name','email_id','user_type','user_status') 
-
-class UserLoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields =  ('first_name','last_name','email_id','user_type')
-
+        fields = ('first_name','last_name','email_id','user_type','user_status')
+        
 class RetriveUsersSerializer(serializers.ModelSerializer):
+    user_type = serializers.SlugRelatedField(
+    read_only=True,
+    slug_field='name')
     class Meta:
         model = User
         fields =  ('id','first_name','last_name','email_id','user_type')
@@ -45,3 +76,17 @@ class UserAdditionalInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAdditionalInfo
         fields = ('date_of_birth','credit_score','annual_salary') 
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    user_type = serializers.SlugRelatedField(
+    read_only=True,
+    slug_field='name')
+    class Meta:
+        model = User
+        fields =  ('first_name','last_name','email_id','user_type')
+
+class UserStatusUpdateSerializer(serializers.ModelSerializer):
+    user_status = UserStatusFieldSerializer()
+    class Meta:
+        model = User
+        fields =  ('id','user_status')
