64ab8329c805ebf2c01afaaf6fee41ef2db766de
Aditya Shah <aditya7395@gmail.com>
2020-11-25 11:40:39 -0800

    Added nav item for mananging listings and added base code for its components

---
+++ frontend/src/User/Body/ViewListings.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import Grids from './Grids/Grids';
+
+function ViewListings(props) {
+    const[homes, setHomes] = useState([]);
+    useEffect(() => {
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      fetch(proxyurl + "https://home-finder-backend-staging.herokuapp.com/listings/1")
+        .then((response) => response.json())
+        .then((data) => setHomes(data));
+    },[]);
+    return (
+      <div>
+        <div>{homes['street_address']}</div>
+      </div>
+    );
+  }
+  
+
+export default ViewListings;
